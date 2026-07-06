dc48ea718990005c1c87c8ff9ef0ee9c86ae11a1
zhongrunqiu <42689111+zhongrunqiu@users.noreply.github.com>
2026-07-06 08:35:05 +0800

    try to fix fingerprint issue

--- fetch.py
+++ fetch.py
@@ -582,6 +582,8 @@ class Node:
         if 'alpn' in ret and isinstance(ret['alpn'], str):
             # 'alpn' is not a slice
             ret['alpn'] = ret['alpn'].replace(' ','').split(',')
+        if 'fingerprint' in ret:
+            del ret['fingerprint']
         return ret
 
     def supports_meta(self, noMeta=False) -> bool:
@@ -1188,6 +1190,8 @@ def main():
     names_clash_meta = list(names_clash_meta)
     conf_meta = copy.deepcopy(conf)
 
+    conf.pop('global-client-fingerprint', None)
+    conf_meta.pop('global-client-fingerprint', None)
     # Clash
     conf['proxies'] = proxies
     for group in conf['proxy-groups']:
